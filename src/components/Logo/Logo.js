f81b3645f20cac90aefb4c7a956f065957ae5ad7
VishalMandalia <vishal.smandalia001@gmail.com>
2020-05-07 14:03:12 +0530

    edits

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,10 +5,10 @@ import './Logo.css';
 
 const Logo = () => {
   return (
-    <div class="align">
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 60 }} style={{ height: 150, width: 150 }} >
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
         <div className="Tilt-inner pa3">
-          <img style={{marginTop: '-11px'}} alt='logo' src={brain}/>
+          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
         </div>
       </Tilt>
     </div>
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './rank.css';
 
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div class="align">
-        {`Hello!! ${name}, your current entry count is...`}
+      <div className='white f3'>
+        {`${name}, your current entry count is...`}
       </div>
       <div className='white f1'>
         {entries}
